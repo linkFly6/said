731522b12efdc7ecdb0e74225c93a14549913e75
[ Dying In The Sun ] <linkFly6@live.com>
2014-11-03 00:12:06 +0800

    微调代码

---
+++ Said.Domain/Said.Entity/Said.Entiry/Said.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Said.Domain.Said.Entity.Said.Entiry
+{
+    /// <summary>
+    /// 领域实体：文章
+    /// </summary>
+    internal class Said
+    {
+        public static void save()
+        {
+
+        }
+    }
+}
