9daf12a2c7306b6aa418fa7c03f464b80133d3ff
[ Dying In The Sun ] <linkFly6@live.com>
2014-11-25 01:57:20 +0800

    编写添加Said页面

--- Said.Infrastructure/Said.Models/Models/Article.cs
+++ Said.Infrastructure/Said.Models/Models/Article.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace Said.Models
         /// <summary>
         /// Said Id
         /// </summary>
+        [Required]
         public string SaidId { get; set; }
         /// <summary>
         /// XML
