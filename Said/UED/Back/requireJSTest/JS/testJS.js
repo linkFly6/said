bb8740e0e8df0d77408f82df14d996b515760eb2
[ Dying In The Sun ] <linkFly6@live.com>
2015-01-10 15:33:43 +0800

    添加Said首页，正在规划首页分布

---
+++ Said/UED/Back/requireJSTest/JS/testJS.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    var test = {
+        log: function () {
+            console.log('linkFly');
+        }
+    };
+    window.test = test;
+    return test;
+})();
+define('Defer', function () {
+    return {
+        log: function () {
+            console.log('defer');
+        }
+    };
+});
