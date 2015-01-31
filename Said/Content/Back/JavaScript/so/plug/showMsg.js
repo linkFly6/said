4c870b4b5fea497ae624f7c56d0d683bdd68e84c
[ Dying In The Sun ] <linkFly6@live.com>
2015-02-01 00:48:05 +0800

    showMsg.js编写完毕

--- Said/Content/Back/JavaScript/so/plug/showMsg.js
+++ Said/Content/Back/JavaScript/so/plug/showMsg.js
@@ -2,11 +2,8 @@
     // Client-side export
     if (typeof window === 'undefined' || !window.so) return;
     var so = window.so,
-        templent = '<div class="showmsg-container">\
-            <div class="showmsg-mask"></div>\
-                <div class="showmsg-content">\
-                    <img class="showmsg-state" src="~/Content/Back/Images/showMsg/ok.png" />\
-                <div class="showmsg-context">${0}</div></div></div>',
+        templent = '<div class="showmsg-container">${0}\
+                <div class="showmsg-content ${1}">${2}<div class="showmsg-context">${3}</div></div></div>',
         globalConfig = {
             cache: true,//是否缓存这次结果
             title: null,
@@ -23,26 +20,86 @@
             }, time);
             return id;
         },
-        append = function () {
-            var body = window.document.body;
-            return function (elem) {
-                body.appendChild(elem);
-            }
-        }(),
+        body = window.document.body,
+        append = function (elem) {
+            body.appendChild(elem);
+            return elem;
+        },
+        remove = function (elem) {
+            return body.removeChild(elem);
+        },
         parseHTML = function (html) {
             var doc = document.createElement('div');
             doc.innerHTML = html;
             return doc.firstElementChild;
         },
-        State = {
-            OK: 0,
-            ERROR: 1,
-            WARNING: 2
-        };
+        getScrollTop = function () {
+            return window.pageYOffset;
+            //|| window.scrollY ||//ff&chrome
+            //document.documentElement.scrollTop;//ie
+        },
+        setPos = function (elem) {
+            var width = elem.clientWidth + 6,//border=>3px
+                height = elem.clientHeight + 6 + 200,//+200是为了美观，偏上一丢丢...
+                innerHeight = window.innerHeight,//视口高度
+                innerWidth = window.innerWidth,//视口宽度
+                scrollTop = getScrollTop();//滚动条滚动高度，如果考虑横向滚动条的话则需要再计算横向滚动条卷去的宽度
+            //955
+            elem.style.top = (innerHeight - height) / 2 + scrollTop + 'px';
+            elem.style.left = (innerWidth - width) / 2 + 'px';
+        },
+        stateFlags = 'OK ERROR WARNING',
+        stateNames = [],
+        State = {},
+        setGlobalShowMsg = function () {
+            var currentShowMsg;
+            return function (showMsgObject) {
+                if (currentShowMsg)
+                    currentShowMsg.clear();
+                currentShowMsg = showMsgObject;
+            };
+        }();
     var ShowMsg = function (text, state, time) {
+        if (!(this instanceof ShowMsg))
+            return new ShowMsg(text, state, time);
         if (!text) return;
-        var html = [];
+        if (state === true) {
+            time = true;
+            state = null;
+        }
+        var formatStrs = [time === true ? '<div class="showmsg-mask"></div>' : ''],
+            stateName = state != null && stateNames[state] || '',
+            elem;
+        stateName ?
+            formatStrs.push('showmsg-state-' + stateName, '<img class="showmsg-state" src="/Content/Back/Images/showMsg/' + stateName + '.png" />') :
+            formatStrs.push('', '');
+        formatStrs.push(text);
+        elem = this.elem = parseHTML(so.format(templent, formatStrs[0], formatStrs[1], formatStrs[2], formatStrs[3]));
+        if (time === true) {
+            elem.firstElementChild.addEventListener('click', function () {
+                setGlobalShowMsg(null);
+            });
+        }
+        append(elem);
+        setPos(elem.lastElementChild);
+        setGlobalShowMsg(this);
+        if (time === true) return;
+        this.timeoutId = timeout(function () {
+            setGlobalShowMsg(null);
+        }, typeof time === 'number' && time > 0 ? time : ShowMsg.time);
     };
+    ShowMsg.prototype.clear = function () {
+        clearTimeout(this.timeoutId);
+        remove(this.elem);
+    };
+    //状态标识
+    stateFlags.split(' ').forEach(function (str, i) {
+        var state = str.split(' ');
+        State[state[0]] = i;
+        ShowMsg[state[0]] = i;
+        stateNames[i] = state[0].toLowerCase();
+    });
+    ShowMsg.time = 3000;
     window.showMsg = ShowMsg;
     //兼容amd
     if (typeof define === "function" && define.amd) {
