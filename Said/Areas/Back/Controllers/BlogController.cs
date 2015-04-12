5c94747ac0a1f5beaa4c68da597b0e7ed65d434b
[ Dying In The Sun ] <linkFly6@live.com>
2015-04-12 22:09:59 +0800

    Said后台管理系统考虑到业务复杂度，加入MVVM框架，测试了KnockOutJS和Avalon，调整为采用Avalon

---
+++ Said/Areas/Back/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Said.Areas.Back.Controllers
+{
+    public class BlogController : Controller
+    {
+        //
+        // GET: /Back/Blog/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddBlog()
+        {
+            return View();
+        }
+
+    }
+}
