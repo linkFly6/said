d66c3be1ed468580a457979ed7be0df968f72e04
linkFly <linkFly6@live.com>
2015-08-11 00:11:07 +0800

    编写IP白名单

--- Said/Common/IPRange.cs
+++ Said/Common/IPRange.cs
@@ -9,11 +9,11 @@ namespace Said.Common
 {
     public class IPRange
     {
-        private static string[][] ips;
+        private static uint[][] ips;
         /// <summary>
         /// IP段(交错数组)
         /// </summary>
-        public static string[][] IPs
+        public static uint[][] IPs
         {
             get
             {
@@ -27,23 +27,32 @@ namespace Said.Common
         /// 读取IP段配置
         /// </summary>
         /// <returns></returns>
-        private static string[][] Load()
+        private static uint[][] Load()
         {
             if (IPs != null) return IPs;
             //172.0.0.1;192.168.1.1-192.168.1.2
             string[] ipStr = ConfigTable.Get(ConfigEnum.IPRange).Split(';');
-            ips = new string[ipStr.Length][];
+            ips = new uint[ipStr.Length][];
             for (int i = 0; i < ipStr.Length; i++)
             {
-                //ips[i] = ipStr[i].Split('-');
-                //ips[i] = ipStr[i].Split('-')[0];
-                UInt32.Parse(IPAddress.Parse("").GetAddressBytes());
+                string[] temp = ipStr[i].Split('-');
+                uint startIP = BitConverter.ToUInt32(IPAddress.Parse(temp[0]).GetAddressBytes(), 0),
+                    endIP = 0;
+                //这里还要判定127.0.*.*和127.0.*.*-127.22.*.*
+
+                if (temp.Length > 1) endIP = BitConverter.ToUInt32(IPAddress.Parse(temp[1]).GetAddressBytes(), 0);
+                //http://stackoverflow.com/questions/461742/how-to-convert-an-ipv4-address-into-a-integer-in-c
+                if (endIP == 0)
+                    ips[i] = new uint[1] { startIP };
+                else
+                    ips[i] = new uint[2] { startIP, endIP };
             }
-
-            System.Net.IPAddress.Parse("").GetAddressBytes();
             return ips;
-
         }
+
+        //private static uint ip2Uint() { 
+
+        //}
 
 
         /// <summary>
@@ -53,9 +62,16 @@ namespace Said.Common
         /// <returns></returns>
         public static bool Check(string ip)
         {
-            
-            if (ip == "::1") return true;
 
+            if (ip == "::1") return true;
+            uint numberIP = BitConverter.ToUInt32(IPAddress.Parse(ip).GetAddressBytes(), 0);
+            foreach (uint[] item in ips)
+            {
+                if (item.Length > 1 && item[0] <= numberIP && numberIP <= item[1])
+                    return true;
+                else if (item[0] == numberIP)
+                    return true;
+            }
             return false;
         }
     }
