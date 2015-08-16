5179df431a71e865af7f9575a39d4bb8cd3aebd7
linkFly <linkFly6@live.com>
2015-08-16 21:19:49 +0800

    编写站点配置

---
+++ Said/Areas/Back/Controllers/SiteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Said.Areas.Back.Controllers
+{
+    public class SiteController : BaseController
+    {
+        //
+        // GET: /Back/Site/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult PageConfig()
+        {
+            return View();
+        }
+    }
+}
