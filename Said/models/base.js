49f958363d5377a3a31e19c2721c260edcc8a5ed
linkFly <linkFly6@live.com>
2015-09-13 23:48:00 +0800

    连接mongooDB测试

---
+++ Said/models/base.js
@@ -0,0 +1,4 @@
+﻿exports.BaseSchema = function () {
+
+};
+
---
+++ Said/models/database.js
@@ -0,0 +1,30 @@
+﻿var mongoose = require('mongoose');
+var db = mongoose.connect('localhost', 'said');
+db.on('error', function (error) {
+    console.log(error);
+});
+
+var tagSchema = new mongoose.Schema({
+    tagName: String,
+    date: Date,
+    count: Number,
+    isDelete: Number
+}, {
+    collection: 'tags'
+});
+
+exports.tagSchema = tagSchema;
+exports.tagModel = mongoose.model('Tag', userSchema);
+
+//process.on('SIGINT', function () {
+//    mongoose.connection.close(function () {
+//        console.log('Mongoose disconnected through app termination');
+//        process.exit(0);
+//    });
+//});
+
+//module.exports = function (callback) {
+//    db.once('open', function () {
+
+//    });
+//};
