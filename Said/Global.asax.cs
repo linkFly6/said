a1debeda46432fffebd35e8e9cec0842887c9816
linkFly <linkFly6@live.com>
2016-01-06 21:03:38 +0800

    加入更详细的log

--- Said/Global.asax.cs
+++ Said/Global.asax.cs
@@ -4,6 +4,7 @@ using Said.Config;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -17,19 +18,33 @@ namespace Said
     {
         protected void Application_Start()
         {
+            MvcHandler.DisableMvcResponseHeader = false;
+            LogCommon.Log(string.Format("应用程序开始"));
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
+            LogCommon.Log(string.Format("载入配置文件"));
             //加载配置文件
             ConfigTable.LoadConfig(Server.MapPath("~/config.json"));
+            StringBuilder sb = new StringBuilder("载入配置文件成功\n");
+            sb.AppendLine("配置文件内容：\n");
+            foreach (string key in ConfigTable.Table.Config.Keys)
+            {
+                sb.AppendFormat("{0} : {1} \n", key, ConfigTable.Table.Config[key]);
+            }
+            LogCommon.Log(sb.ToString());
+
             /*默认加载IP查询库*/
+            LogCommon.Log(string.Format("载入IP查询库"));
             Said.Helper.IP.Load(Server.MapPath(ConfigTable.Table[ConfigEnum.SourceDataIP]));
 
 
             //启动的时候创建所有路径
+            LogCommon.Log("开始创建资源文件夹");
+            LogCommon.Log(string.Format("测试文件夹路径：{0}", ConfigInfo.SourceBlogPath));
             FileCommon.ExistsCreate(Server.MapPath(ConfigInfo.SourceBlogPath));
             FileCommon.ExistsCreate(Server.MapPath(ConfigInfo.SourceBlogThumbnailPath));
             FileCommon.ExistsCreate(Server.MapPath(ConfigInfo.SourceIconsPath));
@@ -38,6 +53,7 @@ namespace Said
             FileCommon.ExistsCreate(Server.MapPath(ConfigInfo.SourceSaidThumbnailPath));
             FileCommon.ExistsCreate(Server.MapPath(ConfigInfo.SourceSystemPath));
             FileCommon.ExistsCreate(Server.MapPath(ConfigInfo.SourceSystemThumbnailPath));
+            LogCommon.Log("资源文件夹创建完毕");
         }
 
         protected void Application_Error(object sender, EventArgs e)
