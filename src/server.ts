be55cf6cb4668691339f13e572647ffe26f23b7d
linkFly <linkFly6@live.com>
2018-03-06 20:32:32 +0800

    done

--- src/server.ts
+++ src/server.ts
@@ -253,29 +253,26 @@ app.use(homeController.noFound)
 if (process.env.NODE_ENV === 'production') {
   // 线上启用 HTTPS
   const lex = greenlock.create({
-    server: 'staging',
-    // server: 'https://acme-v01.api.letsencrypt.org/directory',
+    // server: 'staging', // 测试
+    server: 'https://acme-v01.api.letsencrypt.org/directory', // 生产
     challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '~/letsencrypt/var/acme-challenges' }) },
     store: require('le-store-certbot').create({ webrootPath: '~/letsencrypt/srv/www/:hostname/.well-known/acme-challenge' }),
-    // agreeTos: true,
     approveDomains: (opts: any, certs: any, cb: any) => {
       appLog.info('approveDomains', { opts, certs })
       if (certs) {
         opts.domains = certs.altnames
-      }
-      else {
+      } else {
         opts.email = 'linkFly6@live.com'
         opts.agreeTos = true
       }
       cb(null, { options: opts, certs: certs })
     },
-    // app,
   })
   require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
-    console.log('Listening for ACME http-01 challenges on', this.address())
+    appLog.info('Listening', `for ACME http-01 challenges on: ${JSON.stringify(this.address())}`)
   })
   require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(443, function () {
-    console.log('Listening for ACME tls-sni-01 challenges and serve app on', this.address())
+    appLog.info('Listening https', `for ACME tls-sni-01 challenges and serve app on: ${JSON.stringify(this.address())}`)
   })
 } else {
   /**
